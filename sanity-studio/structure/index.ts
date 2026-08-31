b14a9d6470f18624e8ec2665ff98b7243f9ea655
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-09-01 00:37:38 +0200

    sanity: enforce siteMedia singleton structure

--- sanity-studio/structure/index.ts
+++ sanity-studio/structure/index.ts
@@ -1,14 +1,23 @@
 import type {StructureResolver} from 'sanity/structure'
 
+export const SITE_MEDIA_ID = 'siteMedia'
+export const singletonTypes = new Set(['siteMedia'])
+
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
     .items([
+      S.listItem()
+        .title('Website Media')
+        .id(SITE_MEDIA_ID)
+        .child(
+          S.document()
+            .schemaType('siteMedia')
+            .documentId(SITE_MEDIA_ID)
+            .title('Website Media')
+        ),
+      S.divider(),
       S.documentTypeListItem('post').title('Posts'),
       S.documentTypeListItem('category').title('Categories'),
       S.documentTypeListItem('author').title('Authors'),
-      S.divider(),
-      S.listItem()
-        .title('Website Media')
-        .child(S.document().schemaType('siteMedia').documentId('siteMedia').title('Website Media')),
     ])
