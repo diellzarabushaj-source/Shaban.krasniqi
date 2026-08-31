7a0cde83fd94d7abc1ce34af87a26e451e861e34
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-09-01 00:36:34 +0200

    Register existing schemas and new siteMedia schema

---
+++ sanity-studio/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import post from './post'
+import category from './category'
+import author from './author'
+import siteMedia from './siteMedia'
+
+export const schemaTypes = [post, category, author, siteMedia]
