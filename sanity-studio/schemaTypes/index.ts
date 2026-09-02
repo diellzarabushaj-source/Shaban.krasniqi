3691cdcc1733cbbda9e4bb8dc3ba9c321aa3401a
diellzarabushaj-source <diellzarabushaj@gmail.com>
2026-09-02 16:31:00 +0200

    Register siteSettings schema

--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -3,5 +3,6 @@ import category from './category'
 import author from './author'
 import seo from './seo'
 import siteMedia from './siteMedia'
+import siteSettings from './siteSettings'
 
-export const schemaTypes = [post, category, author, seo, siteMedia]
+export const schemaTypes = [post, category, author, seo, siteMedia, siteSettings]
